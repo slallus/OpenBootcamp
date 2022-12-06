f26a0c69ebf49cac2b12a7120c351d2a0dbf850d
Steven <112867543+slallus@users.noreply.github.com>
2022-12-06 10:41:40 +0100

    Create Ejercicio5.js

---
+++ Javascript-Basico/Ejercicio-5/Ejercicio5.js
@@ -0,0 +1,28 @@
+//EJERCICIO 5
+
+// - Una variable que contenga tu altura en centímetros (entero)
+
+let altura1 = 182;
+
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+
+let altura2 = 1.82;
+
+
+// - Una variable que contenga tu peso en kilogramos (número de coma flotante)
+
+let peso = 95.99;
+
+// - Una variable que contenga tu altura en metros redondeada hacia arriba
+
+let alturaRedondeada = Math.round(altura2);
+
+// - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+
+let pesoRedondeadoAbajo = Math.floor(peso);
+
+// - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual 
+//   al máximo valor que se puede obtener en Javascript
+
+let max = Number.MAX_VALUE +1;
+let iguales = max === Number.MAX_VALUE;
